c6464b35d56da15be6bf9d4ebb1fa926e21e57ec
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-07-27 19:51:10 +0300

    Add Summary in preview & Edit Details Style

--- src/components/calender-spread.vue
+++ src/components/calender-spread.vue
@@ -1,34 +1,32 @@
-
 <template>
   <button class="x-btn" @click.prevent="$emit('closeCalendar')">x</button>
-  <DatePicker :columns="2" v-model="date" is-range update-on-input/>
+  <DatePicker :columns="2" v-model="date" is-range update-on-input />
 </template>
-    <script>
-    import { Calendar, DatePicker } from 'v-calendar';
-    
-    export default {
-      name:'calendar-spread',
-      components: {
-        Calendar,
-        DatePicker,
-      },
-      props: {
-        // startDate: Object
-      },
-      data() {
-        return {
-          date: null,
-        }
-      },
-      created() {
-        // this.date = this.startDate || new Date()
-      },
-      watch: { 
-        date(){
-          this.$emit('dateChange',this.date)
-        }
-      },
-      methods: {
-      },
-    }
-    </script>
+<script>
+import { Calendar, DatePicker } from "v-calendar";
+
+export default {
+  name: "calendar-spread",
+  components: {
+    Calendar,
+    DatePicker,
+  },
+  props: {
+    // startDate: Object
+  },
+  data() {
+    return {
+      date: null,
+    };
+  },
+  created() {
+    // this.date = this.startDate || new Date()
+  },
+  watch: {
+    date() {
+      this.$emit("dateChange", this.date);
+    },
+  },
+  methods: {},
+};
+</script>
