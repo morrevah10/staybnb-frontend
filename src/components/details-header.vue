7bffa913f8d43fa91b45bc4219b2cfd53784e4f7
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-07-27 17:59:22 +0300

    Add Details Cmps

---
+++ src/components/details-header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="stay-info">
+    <h2 class="stay-name">{{ stay.name }}</h2>
+    <section class="short-info flex">
+      <section class="flex">
+        <img class="star" src="../styles/icons/star.svg" />
+        <h5 class="reviews-rating">4.7 ·</h5>
+        <h5 class="reviews-number">{{ stay.numOfReviews }} reviews</h5>
+        <pre> .</pre>
+        <h5 class="location">
+          {{ stay.address.city }},
+          {{ stay.address.country }}
+        </h5>
+      </section>
+      <section class="flex">
+        <div class="share-container">
+          <button class="info-btn">
+            <img
+              class="share-icon"
+              src="../styles/icons/details-icons/share-icon.svg"
+              alt=""
+            />
+            <h5 class="share-btn">Share</h5>
+          </button>
+        </div>
+        <div class="save-container">
+          <button class="info-btn">
+            <span class="save-icon">♡</span>
+            <h5 class="save-btn">Save</h5>
+          </button>
+        </div>
+      </section>
+    </section>
+    <section class="details-gallery grid">
+      <img
+        :src="`../../img/${imgUrl}`"
+        v-for="(imgUrl, idx) in stay.imgUrls"
+        :key="idx"
+        class="img-gallery"
+      />
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    stay: Object,
+  },
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  created() {},
+  unmounted() {},
+};
+</script>
---
+++ src/views/trips-page.vue
@@ -0,0 +1,99 @@
+<template>
+  <app-header class="details-header header-layout main-layout" />
+  <section class="my-trips main-layout">
+    <h1 class="trips-header">My Trips</h1>
+    <div class="trips-table">
+      <section v-if="trips" class="trips-list">
+        <ul class="dash-table-title">
+          <li>Date</li>
+          <li>Host</li>
+          <li>Stay</li>
+          <li>Dates</li>
+          <li>Nights</li>
+          <li>Guests</li>
+          <li>Price/night</li>
+          <li>Total</li>
+          <li>Status</li>
+          <li>Actions</li>
+        </ul>
+
+        <div class="trips-container" v-for="trip in trips" :key="trip._id">
+          <ul class="order-line">
+            <li>{{ trip.Date }}</li>
+            <li>{{ trip.Host }}</li>
+            <li>{{ trip.Stay }}</li>
+            <li>{{ trip.Dates }}</li>
+            <li>{{ trip.Nights }}</li>
+            <li>{{ trip.Guests }}</li>
+            <li>{{ trip.Price }}</li>
+            <li>{{ trip.Total }}</li>
+            <li>{{ trip.Status }}</li>
+            <li>{{ trip.Actions }}</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  </section>
+  <app-footer class="footer footer-layout main-layout" />
+</template>
+
+<script>
+import appHeader from "../components/app-header.vue";
+import appFooter from "../components/app-footer.vue";
+export default {
+  props: {},
+  components: {
+    appHeader,
+    appFooter,
+  },
+  data() {
+    return {
+      orders: [
+        {
+          _id: "1",
+          Date: "19/07/22",
+          Host: "David",
+          Stay: "Apartment",
+          Dates: "20/07/22 - 22/07/22",
+          Nights: "panding",
+          Guests: "$100",
+          Price: "aprove/decline",
+          Total: "",
+          Status: "",
+          Actions: "",
+        },
+        {
+          _id: "2",
+          Date: "22/07/22",
+          Host: "Daniel",
+          Stay: "Apartment",
+          Dates: "25/07/22 - 26/07/22",
+          Nights: "panding",
+          Guests: "$100",
+          Price: "aprove/decline",
+          Total: "",
+          Status: "",
+          Actions: "",
+        },
+        {
+          _id: "3",
+          Date: "img",
+          Host: "yossi chen",
+          Stay: "2016-05-01",
+          Dates: "2016-05-03",
+          Nights: "panding",
+          Guests: "$100",
+          Price: "aprove/decline",
+          Total: "",
+          Status: "",
+          Actions: "",
+        },
+      ],
+    };
+  },
+  methods: {},
+  computed: {},
+  created() {},
+  unmounted() {},
+};
+</script>
