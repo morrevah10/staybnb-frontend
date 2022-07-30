0f161a87dc6d2467823c5592984893e703bfbf6c
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-07-30 21:26:24 +0300

    fix calende & guest position

--- src/views/reservation-page.vue
+++ src/views/reservation-page.vue
@@ -7,12 +7,12 @@
         <h3 class="reservation-subtitle">Reservation Details:</h3>
       </div>
       <div class="reservation-details">
-        <p>Name :{{loggedinUser.fullName}} </p>
-        <p>Where to : {{currOrder.stayPlace}}</p>
-        <p>Type :{{currOrder.stay}} , {{currOrder.type}}</p>
-        <p>Dates :{{currOrder.checkIn}} - {{currOrder.checkOut}}</p>
-        <p>Guests :{{currOrder.guests}}</p>
-        <p>Price :{{currOrder.total}}</p>
+        <p>Name: {{ loggedinUser.fullName }}</p>
+        <p>Where to: {{ currOrder.stayPlace }}</p>
+        <p>Type: {{ currOrder.stay }} , {{ currOrder.type }}</p>
+        <p>Dates: {{ currOrder.checkIn }} - {{ currOrder.checkOut }}</p>
+        <p>Guests: {{ currOrder.guests }}</p>
+        <p>Price: {{ currOrder.total }}</p>
       </div>
       <router-link :to="'/trips'">
         <button class="trips-btn">Your Trips</button>
@@ -23,8 +23,8 @@
   <app-footer class="footer main-layout" />
 </template>
 <script>
-import appHeader from "../components/app-header.vue"
-import appFooter from "../components/app-footer.vue"
+import appHeader from "../components/app-header.vue";
+import appFooter from "../components/app-footer.vue";
 export default {
   components: {
     appHeader,
@@ -34,21 +34,21 @@ export default {
     return {
       loggedinUser: null,
       currOrder: null,
-    }
+    };
   },
   methods: {},
   computed: {},
   created() {
-    let user = this.$store.getters.loggedinUser
-    console.log("from reservation", user)
-    this.loggedinUser = user
-    console.log("from reservation", this.loggedinUser)
-    console.log("mor revah")
-    let order = this.$store.getters.getCurrOrder
-    console.log("from reservation", order)
-    this.currOrder = order
-    console.log("from reservation", this.currOrder)
+    let user = this.$store.getters.loggedinUser;
+    console.log("from reservation", user);
+    this.loggedinUser = user;
+    console.log("from reservation", this.loggedinUser);
+    console.log("mor revah");
+    let order = this.$store.getters.getCurrOrder;
+    console.log("from reservation", order);
+    this.currOrder = order;
+    console.log("from reservation", this.currOrder);
   },
   unmounted() {},
-}
+};
 </script>
