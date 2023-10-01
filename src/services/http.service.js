cdaedaa3cb101d5b0573354f956f588359d57198
Mor Revah <morrevah10@gmail.com>
2023-10-01 10:53:01 +0300

    .

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 // import { router } from '@/router'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/api/'
+    ? 'https://staybnb-v5m5.onrender.com/api/'
     : '//localhost:3030/api/'
 
 
